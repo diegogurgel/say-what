371f28e3c3ca49c189bc4d3d28829924b182c62c
diego <diego@diegogurgel.com>
2016-07-13 09:53:57 -0300

    change host

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const source = require('vinyl-source-stream');
 gulp.task('webserver', function() {
   gulp.src('app')
     .pipe(webserver({
-      host:'localhost',
+      host:'127.0.0.1',
       port:8081,
       livereload: true,
       open: true
