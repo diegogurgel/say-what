e173fc8c817216bd89536487e71f61996cc2121b
Diego Gurgel <diego.gurgel@sebraemg.com.br>
2016-07-13 14:21:56 -0300

    cache and serve messages

--- app/js/mainController.js
+++ app/js/mainController.js
@@ -1,14 +1,58 @@
 import Posts from './posts.js';
+import idb from '../../node_modules/idb/lib/idb.js';
+
+function openDatabase(){
+	if(!navigator.serviceWorker) return;
+
+	return idb.open('say-what',1,function(upgrade){
+		var store = upgrade.createObjectStore('messages',{
+			keyPath:'id'
+		});
+	});
+}
+var postsArea = document.querySelector('main>section');
+var posts = new Posts();
 export default function Main(){
-	var posts = new Posts()
-	var postsArea = document.querySelector('main>section');
-	var socket = io.connect('http://0.0.0.0:8080');
-	socket.on('say',msg=>{
-		posts.createPost(postsArea,msg);
-		posts.checkPostsVolume(postsArea);
+	var main = this;
+	if(io){
+	}
+	this._dbPromise = openDatabase();
+	this._showCachedMessages().then(function(){
+		var socket = io.connect('http://0.0.0.0:8080');
+		socket.on('say',msg=>{
+			main._saveMessage(msg);
+			posts.createPost(postsArea,msg);
+			posts.checkPostsVolume(postsArea);
+		});
 	});
 	this._registerSW();
 }
+Main.prototype._showCachedMessages = function(){
+	return this._dbPromise.then(db=>{
+		if(!db) return;
+		var transaction = db.transaction('messages','readwrite');
+		var store = transaction.objectStore('messages');
+		return store.getAll().then(messages=>{
+
+				messages.forEach(msg=>{
+					posts.createPost(postsArea,msg);
+					posts.checkPostsVolume(postsArea);		
+				});
+				postsArea.parentElement.scrollTop=0;
+		})
+
+	});
+}
+
+
+Main.prototype._saveMessage = function(message){
+	this._dbPromise.then(db=>{
+		var transaction = db.transaction('messages','readwrite');
+		var store = transaction.objectStore('messages');
+		store.put(message);
+	});
+}
+
 Main.prototype._registerSW = function(){
 	if(!navigator.serviceWorker) return;
 	navigator.serviceWorker.register('/sw.js').then(function(reg) {
--- app/sw.js
+++ app/sw.js
@@ -1,10 +1,10 @@
-var staticCacheName = 'say-what-v1';
+var staticCacheName = 'say-what-v11';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/',
+        '/index.html ',
         'style/main.css',
         'style/post.css',
         'js/transp/all.js',
@@ -13,6 +13,10 @@ self.addEventListener('install', function(event) {
   );
 });
 self.addEventListener('fetch',function(event){
+  var requestUrl = new URL(event.request.url);
+  if(requestUrl.pathname.startsWith('/images')){
+    return;
+  }
 	event.respondWith(
 		caches.match(event.request).then(function(response){
 			return response || fetch(event.request);
